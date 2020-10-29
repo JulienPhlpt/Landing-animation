711df290fe25b8fa7b0441e62f34c6cb35954a18
Julien Philippot <50954315+JulienPhlpt@users.noreply.github.com>
2020-10-29 15:53:28 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,71 +1,25 @@
-// Responsive navbar
-const navSlide = () => {
-  const burger = document.querySelector(".burger");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelectorAll(".nav-links li");
+const hero = document.querySelector(".hero");
+const slider = document.querySelector(".slider");
+const logo = document.querySelector("#logo");
+const burger = document.querySelector(".burger");
+const headline = document.querySelector(".headline");
 
-  burger.addEventListener("click", () => {
-    // toggle navigation bar
-    nav.classList.toggle("nav-acvtive");
+const tl = new TimelineMax();
 
-    // Links animation
-    navLinks.forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.3
-        }s`;
-      }
-    });
-    // Burger animation
-    burger.classList.toggle("toggle");
-  });
-};
-
-// Section highlight
-const gradient = () => {
-  const sections = document.querySelectorAll("section");
-  const bubble = document.querySelector(".bubble");
-  const gradients = [
-    "linear-gradient(45deg, #FF928B, #FFC6C2)",
-    "linear-gradient(45deg, #FEC3A6, #FFE4D7)",
-    "linear-gradient(45deg, #AED1E6, #D0E4F1)",
-    "linear-gradient(45deg, #B7CE63, #C5D883)",
-  ];
-
-  const options = {
-    threshold: 0.7,
-  };
-
-  let observer = new IntersectionObserver(navCheck, options);
-
-  function navCheck(entries) {
-    entries.forEach((entry) => {
-      const className = entry.target.className;
-      const activeAnchor = document.querySelector(`[data-page=${className}]`);
-      const gradientIndex = entry.target.getAttribute("data-index");
-      const coords = activeAnchor.getBoundingClientRect();
-      const directions = {
-        height: coords.height,
-        width: coords.width,
-        top: coords.top,
-        left: coords.left,
-      };
-      if (entry.isIntersecting) {
-        bubble.style.setProperty("left", `${directions.left}px`);
-        bubble.style.setProperty("top", `${directions.top}px`);
-        bubble.style.setProperty("width", `${directions.width}px`);
-        bubble.style.setProperty("height", `${directions.height}px`);
-        bubble.style.background = gradients[gradientIndex];
-      }
-    });
-  }
-
-  sections.forEach((section) => {
-    observer.observe(section);
-  });
-};
-
-navSlide();
-gradient();
+tl.fromTo(hero, 1, { height: "0%" }, { height: "80%", ease: Power2.easeInOut })
+  .fromTo(
+    hero,
+    1.2,
+    { width: "100%" },
+    { width: "80%", ease: Power2.easeInOut }
+  )
+  .fromTo(
+    slider,
+    1.2,
+    { x: "-100%" },
+    { x: "0%", ease: Power2.easeInOut },
+    "-=1.2"
+  )
+  .fromTo(logo, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
+  .fromTo(burger, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5")
+  .fromTo(headline, 0.5, { opacity: 0, x: 30 }, { opacity: 1, x: 0 }, "-=0.5");
